0fe5e92723d828ac9924231b17a1a4c7db555c2d
burakakdgn <burakakdogann99@gmail.com>
2023-11-03 03:03:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kisileruygulamasi/data/datasource/KisilerDataSource.kt
@@ -0,0 +1,41 @@
+package com.example.kisileruygulamasi.data.datasource
+
+import android.util.Log
+import androidx.navigation.Navigation
+import com.example.kisileruygulamasi.R
+import com.example.kisileruygulamasi.data.entity.Kisiler
+import com.example.kisileruygulamasi.room.KisilerDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class KisilerDataSource(var kdao:KisilerDao) {
+
+    suspend fun kisileriYukle() : List<Kisiler> =
+        withContext(Dispatchers.IO){
+
+            return@withContext kdao.kisileriYukle()
+        }
+
+    suspend fun ara(aramaKelimesi:String): List<Kisiler> =
+        withContext(Dispatchers.IO){
+
+            return@withContext kdao.ara(aramaKelimesi)
+        }
+
+    suspend fun kaydet(kisi_ad:String){
+        val yeniKisi = Kisiler(0,kisi_ad)
+        kdao.kaydet(yeniKisi)
+
+    }
+
+    suspend fun guncelle(kisi_id:Int,kisi_ad:String,){
+        val guncellenenKisi = Kisiler(kisi_id,kisi_ad)
+        kdao.guncelle(guncellenenKisi)
+
+    }
+
+    suspend fun sil(kisi_id:Int){
+        val silinenKisi = Kisiler(kisi_id,"")
+        kdao.sil(silinenKisi)
+    }
+}
---
+++ app/src/main/java/com/example/kisileruygulamasi/di/AppModule.kt
@@ -0,0 +1,37 @@
+package com.example.kisileruygulamasi.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.kisileruygulamasi.data.datasource.KisilerDataSource
+import com.example.kisileruygulamasi.data.repo.KisilerRepository
+import com.example.kisileruygulamasi.room.KisilerDao
+import com.example.kisileruygulamasi.room.VeriTabani
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    @Singleton
+    fun provideKisilerDataSource(kdao:KisilerDao) : KisilerDataSource {
+        return KisilerDataSource(kdao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideKisilerRepository(kds:KisilerDataSource) : KisilerRepository {
+        return KisilerRepository(kds)
+    }
+    @Provides
+    @Singleton
+    fun provideKisilerDao(@ApplicationContext context:Context) : KisilerDao {
+        val vt = Room.databaseBuilder(context,VeriTabani::class.java,"todo.sqlite")
+            .createFromAsset("todo.sqlite").build()
+        return vt.getKisilerDao()
+    }
+}
